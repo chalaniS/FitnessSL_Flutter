2ef5710d5072d2f80d7ddea54f38999613cf6790
chalani <chalasaumya@gmail.com>
2023-10-20 13:34:25 +0530

    map started

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,6 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:fitness/constants.dart';
 import 'package:fitness/screens/splashPage.dart';
 import 'package:flutter/material.dart';
-
 import 'firebase_options.dart';
 
 void main() async {
